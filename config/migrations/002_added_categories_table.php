6963ee80e34eecbebdada9eb2f63dae127a7cbee
real34 <contact@pierre-martin.fr>
2010-01-12 18:49:00 +0100

    Added table structure for categories with an initial categories set

---
+++ config/migrations/002_added_categories_table.php
@@ -0,0 +1,79 @@
+<?php
+class M4b4cb207543c421e80be745d0bda76d4 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	var $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	var $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'categories' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM'),
+				),
+			),
+			'create_field' => array(
+				'recipes' => array(
+					'category_id' => array('type' => 'integer', 'null' => false, 'default' => NULL)
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'categories'
+			),
+			'drop_field' => array(
+				'recipes' => array(
+					'category_id'
+				),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	function after($direction) {
+		if ($direction === 'up') {
+			ClassRegistry::flush();
+			$Category = ClassRegistry::init('Category');
+			$categories = array(
+				array('name' => 'Starters'),
+				array('name' => 'Main Dish'),
+				array('name' => 'Desserts'));
+			$Category->saveAll($categories);
+		}
+		return true;
+	}
+}
+?>
--- config/migrations/map.php
+++ config/migrations/map.php
@@ -2,5 +2,7 @@
 $map = array(
 	1 => array(
 		'001_added_users_recipes_and_ingredients_tables' => 'M4b4ca157c5244c338016697e0bda76d4'),
+	2 => array(
+		'002_added_categories_table' => 'M4b4cb207543c421e80be745d0bda76d4'),
 );
 ?>
