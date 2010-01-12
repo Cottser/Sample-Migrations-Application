fd79b1fd81de58a2c4dbd2307a909fdba34c60ba
real34 <contact@pierre-martin.fr>
2010-01-12 17:40:14 +0100

    Added migrations for the initial database with admin user creation (users, recipes, ingrediants, ingredients_recipes tables)

---
+++ config/migrations/001_added_users_recipes_and_ingredients_tables.php
@@ -0,0 +1,105 @@
+<?php
+class M4b4ca157c5244c338016697e0bda76d4 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	var $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	var $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'ingredients' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM'),
+				),
+				'ingredients_recipes' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'ingredient_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+					'recipe_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM'),
+				),
+				'recipes' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
+					'content' => array('type' => 'text', 'null' => false, 'default' => NULL),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM'),
+				),
+				'users' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
+					'password' => array('type' => 'string', 'null' => false, 'default' => NULL),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'ingredients', 'ingredients_recipes', 'recipes', 'users'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	function after($direction) {
+		if ($direction === 'up') {
+			if (!class_exists('Security')) {
+				App::import('Core', 'Security');
+			}
+			
+			ClassRegistry::flush();
+			$User = ClassRegistry::init('User');
+			$user = array(
+                'User' => array(
+                    'name' => 'admin',
+                    'password' => Security::hash('unsecurepassword', null, true)));
+			$User->save($user);
+		}
+		return true;
+	}
+}
+?>
---
+++ config/migrations/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'001_added_users_recipes_and_ingredients_tables' => 'M4b4ca157c5244c338016697e0bda76d4'),
+);
+?>
